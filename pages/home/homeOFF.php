9519115b24f1a1c236936e40b4cdbec270caca42
Cleyton Nobre <luancleyton51@gmail.com>
2019-12-31 11:41:00 -0400

    rm mPDF

--- pages/home/homeOFF.php
+++ pages/home/homeOFF.php
@@ -4,15 +4,40 @@
         <cite>O concontrole finaceiro é um exercicio diário.</cite>
     </div>
 </div>
+
 <div class='container'>
-    <div class='row col-10 mx-auto my-3'>
-        <div class='col-5'>
-            <p>Organize suas contas para nunca mais texto jafja fafjajfjjaf a fjfjjj ajjajfjafjjaf jajfaf j jafjjafj  afjfjajfajfjajjaafjoqjjqjoamdco</p>
+    <div class='row col-12 mx-auto my-5'>
+        <div class='col-6 text-center my-auto'>
+            <p>Per phasellus at fusce mauris litora quisque blandit metus neque, enim metus tempor ligula curae nam risus at suscipit, lacinia lectus tempor enim adipiscing metus egestas fringilla. </p>
         </div>
-        <div class='col-5 ml-auto'>
+        <div class='col-6'>
             <div class='text-secondary'>
-                <i class="fas fa-chart-pie fa-10x"></i>
+                <img src="img/f2.svg" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div><br>
+    
+    <div class='row col-12 mx-auto my-5'>
+        <div class='col-6'>
+            <div class='text-secondary'>
+                <img src="img/f1.svg" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class='col-6 text-center my-auto'>
+            <p>orci cubilia egestas vel dui conubia rutrum risus, tristique sit vehicula massa rutrum imperdiet adipiscing mollis, placerat inceptos accumsan vehicula torquent hendrerit. 
+sollicitudin vehicula ipsum vivamus hendrerit taciti ultricies habitasse, nam arcu ullamcorper tempor himenaeos curabitur maecenas, porttitor mollis velit odio neque scelerisque.  </p>
+        </div>
+    </div><br>
+    
+    <div class='row col-12 mx-auto my-5'>
+        <div class='col-6 text-center my-auto'>
+            <p>Lorem ipsum sed vel class nisl ut molestie porttitor curae torquent eleifend class suspendisse diam suspendisse tortor, placerat quis eu dapibus vivamus egestas adipiscing enim praesent fringilla commodo rhoncus elit eros. 
+per in tempus integer nam lacinia aliquam, pulvinar curabitur fringilla nunc nostra habitant, odio justo etiam feugiat netus. </p>
+        </div>
+        <div class='col-6'>
+            <div class='text-secondary'>
+                <img src="img/f3.svg" alt="" class="img-fluid">
             </div>
         </div>
     </div>
-</div><br>
+</div>
